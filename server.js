d618f98f50e01ab1b3854e00e1d5362d787b91cd
nuthansathish-max <nuthansathish@gmail.com>
2026-09-12 19:32:14 +0530

    Load isolated WhatsApp automation center

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ express.application.listen=function(...args){const server=http.createServer(this
 await import('./order-hardening.js');
 await import('./analytics-ui.js');
 await import('./advanced-analytics.js');
+await import('./whatsapp-automation.js');
 await import('./authoritative-business-status.js');
 await import('./authoritative-public-order.js');
 await import('./business-account-flow.js');
