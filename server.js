fa7503960726b8c83df8e3b11f2c816f234fa047
nuthansathish-max <nuthansathish@gmail.com>
2026-09-13 12:59:41 +0530

    Fix isolated Billing and POS dashboard navigation

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import http from 'node:http';
 express.application.listen=function(...args){const server=http.createServer(this);return server.listen(...args)};
+await import('./billing-nav.js');
 await import('./order-hardening.js');
 await import('./analytics-ui.js');
 await import('./advanced-analytics.js');
