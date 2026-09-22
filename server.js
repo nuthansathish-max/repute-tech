62a6152df5e2fae33fb22550742c35e250027a64
nuthansathish-max <nuthansathish@gmail.com>
2026-09-22 21:21:04 +0530

    Register staff management routes

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ await import('./billing-invoice-redirect-fix.js');
 await import('./authoritative-business-status.js');
 await import('./authoritative-public-order.js');
 await import('./business-account-flow.js');
+await import('./staff-management.js');
 await import('./admin-auth.js');
 await import('./admin-management.js');
 await import('./onboarding-guard.js');
