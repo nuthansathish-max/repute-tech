e92cff44370d686590b32562e74b317d734d56f7
nuthansathish-max <nuthansathish@gmail.com>
2026-09-26 17:25:29 +0530

    Ensure trial database columns before server startup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import http from 'node:http';
 express.application.listen=function(...args){const server=http.createServer(this);return server.listen(...args)};
+await import('./subscription-trial-columns.js');
 await import('./subscription-plan-sync.js');
 await import('./billing-nav.js');
 await import('./order-hardening.js');
